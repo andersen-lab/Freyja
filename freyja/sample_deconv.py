6e6fffba025867b68c47d9824dcaf443b17b172c
Josh <joshualevy2020@u.northwestern.edu>
2021-10-29 15:16:18 -0500

    adding deconv unit tests

--- freyja/sample_deconv.py
+++ freyja/sample_deconv.py
@@ -25,41 +25,32 @@ def buildLineageMap():
 	return mapDict
 
 
-def build_mix_and_depth_arrays(dirName,depthDir,muts):
-	df_mix = pd.DataFrame()
-	df_depths = pd.DataFrame()
-	fnames = os.listdir(dirName)
-
-	filetype =fnames[0].split('.')[-1]
-	for fn in tqdm(os.listdir(dirName)):
-		df = pd.read_csv(dirName+fn,sep='\t')
-		try:
-			df_depth  = pd.read_csv(depthDir+fn.split(filetype)[0]+'depth',sep='\t',header=None,index_col=1)
-		except:
-			continue
-		df['mutName'] = df['REF'] + df['POS'].astype(str) + df['ALT']### this only works for substitutions, but that's what we get from the usher tree
-		df = df.drop_duplicates(subset='mutName')
-		df.set_index('mutName',inplace=True)
-		keptInds = [dfi for jdi, dfi in  enumerate(df.index) if dfi  in muts]#meh... works but should be improved
-		df_mix = df_mix.append(pd.Series({kI:df.loc[kI,'ALT_FREQ'].astype(float) for kI in keptInds},name=fn))
-		df_depths = df_depths.append(pd.Series({kI:df_depth.loc[int(re.findall(r'\d+',kI)[0]),3].astype(float) for kI in muts},name=fn))
-	return df_mix,df_depths
+def build_mix_and_depth_arrays(fn, depthFn,muts):
+	df = pd.read_csv(fn,sep='\t')
+	df_depth  = pd.read_csv(depthFn,sep='\t',header=None,index_col=1)
+	df['mutName'] = df['REF'] + df['POS'].astype(str) + df['ALT']### this only works for substitutions, but that's what we get from the usher tree
+	df = df.drop_duplicates(subset='mutName')
+	df.set_index('mutName',inplace=True)
+	keptInds = [dfi for jdi, dfi in  enumerate(df.index) if dfi  in muts]#meh... works but should be improved
+	mix = pd.Series({kI:df.loc[kI,'ALT_FREQ'].astype(float) for kI in keptInds},name=fn)
+	depths = pd.Series({kI:df_depth.loc[int(re.findall(r'\d+',kI)[0]),3].astype(float) for kI in muts},name=fn)
+	return mix,depths
 
 
-def reindex_dfs(df_barcodes,df_mix,df_depths):
+def reindex_dfs(df_barcodes,mix,depths):
 	#### reindex everything to match across the dfs
 	df_barcodes = df_barcodes.drop(index=['20A', '20C', '20D', '20H(Beta,V2)', '21C(Epsilon)'])### drop Nextstrain clade names. 
 	df_barcodes= df_barcodes.reindex(sorted(df_barcodes.columns), axis=1)
-	df_mix = df_mix.reindex(df_barcodes.columns, axis=1).fillna(0.)
+	mix = mix.reindex(df_barcodes.columns).fillna(0.)
 
-	df_depths = df_depths.drop(labels=[m0 for m0 in muts if m0 not in df_mix.columns.to_list()],axis=1)# dropping extra sequencing depth info we don't need
-	df_depths = df_depths.reindex(df_barcodes.columns, axis=1).fillna(0.)
-	return df_barcodes,df_mix,df_depths
-
-
+	depths = depths.drop(labels=[m0 for m0 in df_barcodes.columns if m0 not in mix.index.to_list()])# dropping extra sequencing depth info we don't need
+	depths = depths.reindex(df_barcodes.columns).fillna(0.)
+	return df_barcodes,mix,depths
 
 
-def map_to_constellation(vals,sample_strains,mapDict):
+
+
+def map_to_constellation(sample_strains,vals,mapDict):
 	#maps lineage names to constellations
 	localDict = {}
 	for jj,lin in enumerate(sample_strains):
@@ -78,53 +69,35 @@ def map_to_constellation(vals,sample_strains,mapDict):
 				localDict['Other']=vals[jj]
 			else:
 				localDict['Other']+=vals[jj]
-
-	localDict = sorted(localDict.items(), key=lambda x: x[1], reverse=True)#convert to descending order
+	#convert to descending order
+	localDict = sorted(localDict.items(), key=lambda x: x[1], reverse=True)
 	return localDict
 
 
-def solve_demixing_problem(df_barcodes,df_mix,df_depths,mapDict):
-	sols = []
-	errors = []
-	strains = []
-	summarized = []
-	abundances = []
+def solve_demixing_problem(df_barcodes,mix,depths):
+	### single file problem setup, solving
 	eps = 1e-5#minimum abundance to include---TO DO:allow user to modify
-	for i in tqdm(range(df_mix.shape[0])):
-		dep= np.log2(df_depths.iloc[i]+1)
-		dep=dep/np.max(dep)#normalize depth scaling pre-optimization
+	dep= np.log2(depths+1)
+	dep=dep/np.max(dep)#normalize depth scaling pre-optimization
 
-		# set up and solve demixing problem
-		A = np.array((df_barcodes*dep).T)
-		b = np.array(pd.to_numeric(df_mix.iloc[i])*dep)
-		x = cp.Variable(A.shape[1])
-		cost = cp.norm(A @ x - b,1)
-		constraints = [sum(x)==1,x>=0]
-		prob = cp.Problem(cp.Minimize(cost), constraints)
-		prob.solve(verbose=False)
-		sol = x.value
-		rnorm = cp.norm(A @ x - b, 1).value
-		#extract lineages with non-negligible abundance
-		sol[sol<eps] = 0
-		nzInds = np.nonzero(sol)[0]
-		newOrd = np.argsort(sol[nzInds])
-		sample_strains = df_barcodes.index[nzInds].to_list()
-		vals = sol[nzInds]/np.sum(sol[nzInds])
-		strains.append(sample_strains)
-
-		#### summarize by constellation
-		localDict = mapToConstellation(vals,sample_strains,mapDict)
-
-		abundances.append(sol[nzInds])
-		summarized.append(localDict)
-		sols.append(sol)
-		errors.append(rnorm)
-	sols_df = pd.DataFrame(data=np.stack((summarized,strains,abundances,errors),axis=1),index=[dmi.split('.')[0] for dmi in df_mix.index],columns=['summarized','lineages','abundances','resid'])
-	sols_df = sols_df.sort_index()
-	return sols_df
-
-
-
+	# set up and solve demixing problem
+	A = np.array((df_barcodes*dep).T)
+	b = np.array(pd.to_numeric(mix)*dep)
+	x = cp.Variable(A.shape[1])
+	cost = cp.norm(A @ x - b,1)
+	constraints = [sum(x)==1,x>=0]
+	prob = cp.Problem(cp.Minimize(cost), constraints)
+	prob.solve(verbose=False)
+	sol = x.value
+	rnorm = cp.norm(A @ x - b, 1).value
+	#extract lineages with non-negligible abundance
+	sol[sol<eps] = 0
+	nzInds = np.nonzero(sol)[0]
+	newOrd = np.argsort(sol[nzInds])
+	sample_strains = df_barcodes.index[nzInds].to_list()
+	# vals = sol[nzInds]/np.sum(sol[nzInds])### that 
+	abundances = sol[nzInds]
+	return sample_strains,abundances,rnorm
 
 if __name__ == '__main__':
 	print('loading lineage models')
@@ -139,10 +112,17 @@ if __name__ == '__main__':
 	### grab wastewater sample data
 	####################
 
-	dirName = sys.argv[1]#variant files dir
-	depthDir = sys.argv[2]#depth files dir
+	varFn = sys.argv[1]#variant file
+	depthFn = sys.argv[2]#depth file
 	### assemble data from of (possibly) mixed samples
-	df_mix,df_depths = build_mix_and_depth_arrays(dirName,depthDir,muts)
+	df_mix,df_depths = build_mix_and_depth_arrays(varFn,depthFn,muts)
 	print('demixing')
-	sols_df = solve_demixing_problem(df_barcodes,df_mix,df_depths)
+	sample_strains,abundances,error = solve_demixing_problem(df_barcodes,df_mix,df_depths)
+	localDict = mapToConstellation(sample_strains,abundances,mapDict)
+
+	### assemble into df and write. 
+	sols_df = pd.DataFrame(data=np.stack((localDict,sample_strains,abundances,error),axis=1),\
+	index=[dmi.split('.')[0] for dmi in df_mix.index],\
+	columns=['summarized','lineages','abundances','resid'])
+	sols_df = sols_df.sort_index()
 	sols_df.to_csv(sys.argv[3],sep='\t')
--- freyja/tests/test_deconv.py
+++ freyja/tests/test_deconv.py
@@ -1,14 +1,50 @@
 import unittest
 from freyja.sample_deconv import *
 import pandas.testing as pdt
+import pandas.api.types as ptypes
+from numpy.random import negative_binomial
 
 class DeconvTests(unittest.TestCase):
     def test_buildLineageMap(self):
         mapDict = buildLineageMap()
         self.assertTrue('Alpha'==mapDict['B.1.1.7'] and 'Delta'==mapDict['AY.4'])
 
-    # def test_build_mix_and_depth(self):
-        
+    def test_build_mix_and_depth_plus_reindex(self):
+        df_barcodes = pd.read_csv('freyja/data/usher_barcodes.csv',index_col=0)
+        muts = list(df_barcodes.columns)
+        varFn='freyja/data/mixture.tsv'
+        depthFn='freyja/data/mixture.depth'
+        mix,depths = build_mix_and_depth_arrays(varFn,depthFn,muts)
+        #just making sure the files we've read in are ready for use
+        self.assertTrue(ptypes.is_float_dtype(mix))
+        self.assertTrue(ptypes.is_float_dtype(depths)) 
+
+        df_barcodes,mix,depths = reindex_dfs(df_barcodes,mix,depths)
+        pdt.assert_index_equal(df_barcodes.columns,mix.index)
+        pdt.assert_index_equal(df_barcodes.columns,depths.index)
+
+        self.assertFalse('20C' in df_barcodes.columns)
+
+    def test_constellation_mapping(self):
+        mapDict = buildLineageMap()
+        vals = [0.1,0.5,0.31,0.01,0.02,0.01]
+        strains = ['B.1.617.2','Q.3','B.1.427','A.2.5','B.1.1','B.1.1.7']
+        locDict = map_to_constellation(strains,vals,mapDict)
+        locDict_ideal = {'Alpha':0.51,'Epsilon':0.31,'Delta':0.1,'Other':0.02,'Aaron':0.01}
+        self.assertTrue(locDict==list(locDict_ideal.items()))
+
+    def test_demixing(self):
+        df_barcodes = pd.read_csv('freyja/data/usher_barcodes.csv',index_col=0)
+        #build crude in silico mixture from barcodes, perform recovery
+        strain1='B.1.1.7'
+        strain2='B.1.427'
+        mixFracs = [0.4,0.6]
+        eps = 0.001 #to check accuracy
+        mix =mixFracs[0]*df_barcodes.loc[strain1,]+mixFracs[1]*df_barcodes.loc[strain2,]
+        depths = negative_binomial(50,0.25,size=len(mix))
+        sample_strains,abundances,error = solve_demixing_problem(df_barcodes,mix,depths)
+        self.assertTrue(np.abs(abundances[sample_strains.index(strain1)]-mixFracs[0]<eps))
+        self.assertTrue(np.abs(abundances[sample_strains.index(strain2)]-mixFracs[1]<eps))
 
 
 if __name__ == '__main__':
