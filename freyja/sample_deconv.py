ebdef51b7175ff4644a155e147ca3880575e9da5
tkchafin <tylerkchafin@gmail.com>
2023-05-22 19:44:44 +0100

    fixed imports and linting errors

--- freyja/sample_deconv.py
+++ freyja/sample_deconv.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 from joblib import Parallel, delayed
 from tqdm import tqdm
 import matplotlib
+import glob
+from collections import defaultdict
+
 
 def buildLineageMap(locDir):
     # Parsing curated lineage data from outbreak.info
@@ -16,29 +19,32 @@ def buildLineageMap(locDir):
     if os.path.isdir(locDir):
         global_dat = defaultdict(set)
         for filename in glob.glob(os.path.join(locDir, "*.json")):
-            with open(filename) as f0:
+            with open(filename, "r") as f0:
                 dat = json.load(f0)
                 for record in dat:
-                    if 'who_name' in record.keys():
-                        if record['who_name'] is not None:
-                            global_dat[record['who_name']].update(record.get('pango_descendants', []))
-
-        sorted_dat = sorted(global_dat.items(), key=lambda x: len(x[1]), reverse=False)
-
+                    if 'who_name' in record:
+                        global_dat[record['who_name']].update(
+                            record.get('pango_descendants', []))
+        # Sort global_dat by number of descendants (largest first)
+        sorted_dat = sorted(global_dat.items(),
+                            key=lambda x: len(x[1]),
+                            reverse=True)
         for clade, descendants in sorted_dat:
             for descendant in descendants:
-                if descendant not in mapDict:
-                    mapDict[descendant] = clade 
+                mapDict[descendant] = clade
     else:
         if locDir == '-1':
-            locDir = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
-            with open(os.path.join(locDir, 'data/curated_lineages.json')) as f0:
+            locDir = os.path.abspath(
+                os.path.join(os.path.realpath(__file__), os.pardir))
+            with open(os.path.join(
+                    locDir, 'data/curated_lineages.json')) as f0:
                 dat = json.load(f0)
         else:
             with open(locDir) as f0:
                 dat = json.load(f0)
         # Sort records based on the length of 'pango_descendants'
-        dat = sorted(dat, key=lambda x: len(x.get('pango_descendants', [])), reverse=True)
+        dat = sorted(dat, key=lambda x: len(
+            x.get('pango_descendants', [])), reverse=True)
         for ind in range(len(dat)):
             if 'who_name' in dat[ind].keys():
                 for d0 in dat[ind]['pango_descendants']:
