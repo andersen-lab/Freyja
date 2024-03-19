86a0fa540b33b0ad8700d0a04177b5a47d36b4ad
dylanpilz <dpilz@ucsd.edu>
2024-03-19 14:42:16 -0700

    retry rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ release = '1.5.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_click']
+extensions = ['sphinx_click', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
