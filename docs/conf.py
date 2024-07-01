bd73a4696bcd4d8cb89f78ec0d91bde4beed5321
joshuailevy <joshua.levy16@gmail.com>
2024-07-01 14:30:35 -0700

    update version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import os
 project = 'Freyja'
 copyright = '2024, Andersen Lab'
 author = 'Andersen Lab'
-version = 'v1.5.0'
+version = 'v1.5.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
