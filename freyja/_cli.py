7da221fe57f5bce2b69f26300e87dd16e41515ac
Josh Levy <joshua.levy16@gmail.com>
2025-09-03 17:06:19 -0700

    Update version number

--- freyja/_cli.py
+++ freyja/_cli.py
@@ -13,7 +13,7 @@ pathogens = ['SARS-CoV-2'] + list(pathogen_config.keys())
 
 
 @click.group(context_settings={'show_default': True})
-@click.version_option('2.0.0')
+@click.version_option('2.0.1')
 def cli():
     pass
 
