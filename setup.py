d3f6a0abbb2f478ee47185c10f5bcc3f0e6dcfb7
joshuailevy <joshua.levy16@gmail.com>
2026-05-27 17:14:02 -0700

    add plotly dep to setup

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     install_requires=[
         "click", "numpy", "pandas", "cvxpy", "seaborn", "pysam",
         "biopython", "sphinx", "sphinx_rtd_theme","pyarrow", "tqdm",
+        "plotly",
         "sphinx-click @ git+https://github.com/dylanpilz/sphinx-click.git"
         ]
 )
